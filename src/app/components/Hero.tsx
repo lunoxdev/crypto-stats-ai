28bc5219884301cdb23633ae451672dde519789c
lunoxdev <lunox.code@gmail.com>
2025-09-14 19:30:54 -0600

    add: Acerternity Sparkles Component on Hero section

---
+++ src/app/components/Hero.tsx
@@ -0,0 +1,33 @@
+import { SparklesCore } from "@/components/ui/sparkles";
+
+const Hero = () => {
+    return (
+        <section className="h-1/3 w-full bg-black flex flex-col items-center justify-center overflow-hidden rounded-md">
+            <h1 className="text-5xl sm:text-6xl font-semibold relative z-20">
+                Crypto Graph
+            </h1>
+            <div className="w-[45rem] h-40 relative">
+                {/* Gradients */}
+                <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-[2px] w-3/4 blur-sm" />
+                <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-px w-3/4" />
+                <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-[5px] w-1/4 blur-sm" />
+                <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-px w-1/4" />
+
+                {/* Core component */}
+                <SparklesCore
+                    background="transparent"
+                    minSize={0.4}
+                    maxSize={1}
+                    particleDensity={1200}
+                    className="w-full h-full"
+                    particleColor="#FFFFFF"
+                />
+
+                {/* Radial Gradient to prevent sharp edges */}
+                <div className="absolute inset-0 w-full h-full bg-black [mask-image:radial-gradient(350px_200px_at_top,transparent_20%,white)]"></div>
+            </div>
+        </section>
+    )
+}
+
+export default Hero
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import Hero from "./components/Hero";
+
 export default function Home() {
   return (
-    <main className="flex items-center justify-center w-full">
-      <h1>Crypto Graph</h1>
+    <main className="flex flex-col items-center max-w-7xl w-full h-full mx-auto p-4">
+      {/* Hero Section with Aceternity Sparkles Component */}
+      <Hero />
     </main>
   );
 }
